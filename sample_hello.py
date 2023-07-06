76dbffe2d1f772eff6caa7ce705001242f4989a6
RymmaKhlaponina <rymma.khlaponina@gmail.com>
2023-07-06 12:22:54 +0200

    Initial commit

---
+++ sample_hello.py
@@ -0,0 +1,6 @@
+""" First sample. Using print(), input() functions.
+1) Declare a variable and write a string value to it from the console
+2) Output the welcome string """
+
+user_name = str(input("Write your name: "))
+print("Hello " + user_name + '!')
